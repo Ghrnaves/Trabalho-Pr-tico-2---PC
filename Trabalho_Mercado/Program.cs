996953f6c5a5a1e56f6046bf5fd0bc8152bd1dc1
Ghrnaves <gustavohr.naves@gmail.com>
2026-06-10 16:59:36 -0300

    Fiz o menu e separei ja as funcoes que a gente vai precisar e sai criando os vetores que a gente vai precisar e coloquei o tamanhno maximo como 100 neles, da pra alterar isso

--- Trabalho_Mercado/Program.cs
+++ Trabalho_Mercado/Program.cs
@@ -8,9 +8,71 @@ namespace Trabalho_Mercado
 {
     internal class Program
     {
-        static void Main(string[] args)
+        const int MAX = 100;
+        
+        static int[] prodCodigo = new int[MAX];
+        static string[] prodDescricao = new string[MAX];
+        static double[] prodValor = new double[MAX];
+        static int[] prodEstoque = new int[MAX];
+        static int totalProdutos = 0; 
+                                      
+        static int[] vendaProduto = new int[MAX];
+        static int[] vendaFuncionario = new int[MAX];
+        static int[] vendaQuantidade = new int[MAX];
+        static int totalVendas = 0; 
+        static void Main()
         {
-            Console.WriteLine("eh so um teste boy");
+            int opcao;
+            do
+            {
+                Console.WriteLine("========================================");
+                Console.WriteLine("1 - Cadastrar produtos");
+                Console.WriteLine("2 - Realizar uma venda");
+                Console.WriteLine("3 - Relatório de vendas");
+                Console.WriteLine("4 - Relatório de vendas por funcionário");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Selecione uma opção: ");
+
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        CadastrarProduto();
+                        break;
+                    case 2:
+                        RealizarVenda();
+                        break;
+                    case 3:
+                        RelatorioVendas();
+                        break;
+                    case 4:
+                        RelatorioVendasPorFuncionario();
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            } while (opcao != 0);
+        }
+        static void CadastrarProduto()
+        {
+            
+        }
+        static void RealizarVenda()
+        {
+            
+        }
+        static void RelatorioVendas()
+        {
+           
+        }
+        static void RelatorioVendasPorFuncionario()
+        {
+            
         }
     }
 }
+
